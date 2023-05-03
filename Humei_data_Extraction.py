7c2d244cd22ddb7b5ca900754928e21c4930fe5b
Shohreh_Wizard <30856578+shgh24@users.noreply.github.com>
2023-05-03 09:42:05 +0800

    matched the humei data with participant ID and date

--- Humei_data_Extraction.py
+++ Humei_data_Extraction.py
@@ -9,6 +9,7 @@ import glob
 from datetime import datetime
 import pytz
 from pytz import timezone
+import numpy as np
 
 
 # Load credentials and connect to Google Sheets API
@@ -33,10 +34,12 @@ df = pd.DataFrame(data)
 # specify the path to the directory containing the dreem raw files(downloaded from web dashboard)
 dir_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/data_raw/Zepplife/ExtractedFiles'
 dir_path_folders = os.listdir(dir_path)
-Humei_cleaned = pd.DataFrame()
+
+# Humei_finaldata = pd.DataFrame()
+Humei_cleaned_list = []
 
 for path in glob.glob(f'{dir_path}/*/SLEEP*'):
-
+    Humei_cleaned = pd.DataFrame()
     # print(path)
     file_path = glob.glob(f'{path}/*.csv')
     humei_data = pd.read_csv(file_path[0], usecols=[
@@ -44,26 +47,43 @@ for path in glob.glob(f'{dir_path}/*/SLEEP*'):
     # X = df.loc['Session end date']
     # Y = df.loc['Room #']
     roomNo = int(path[-1])
-    wakeUp_time = humei_data['stop']
-    dt = wakeUp_time.apply(
+    # wakeUp_time = humei_data['stop']
+    dt_stop = humei_data['stop'].apply(
         lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S%z'))
 
-    dt_SG = dt.apply(lambda x: x.astimezone(
+    dt_SG_stop = dt_stop.apply(lambda x: x.astimezone(
         timezone('Asia/Shanghai')).strftime('%Y-%m-%dT%H:%M:%S%z'))
-    # humei_data['stop'] = dt_SG.apply(
-    #     lambda x: datetime.strptime(x, '%Y-%m-%d'))
-    humei_data['stop'] = dt_SG.apply(lambda x: x.split('T')[0])
+
+    dt_start = humei_data['start'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S%z'))
+
+    dt_SG_start = dt_start.apply(lambda x: x.astimezone(
+        timezone('Asia/Shanghai')).strftime('%Y-%m-%dT%H:%M:%S%z'))
+
+    humei_data['stop'] = dt_SG_stop
+    humei_data['start'] = dt_SG_start
+    # humei_data['stop'] = dt_SG_stop.apply(lambda x: x.split('T')[0])
+    humei_data['stop_date'] = dt_SG_stop.apply(lambda x: x.split('T')[0])
 
     df['Session end date'] = pd.to_datetime(
         df['Session end date'], format='%Y-%m-%d')
 
     for index, row in humei_data.iterrows():
         match = df[(df['Room #'] == roomNo)
-                   & (row['stop'] == df['Session end date'])]
-        print(match)
+                   & (row['stop_date'] == df['Session end date'])]
+        # print(match)
 
         if not match.empty:
             Humei_cleaned = row
-            Humei_cleaned['Subject'] = match[0].iloc["participant ID"]
+            Humei_cleaned.loc['Subject'] = match["Participant ID"].iloc[0].replace(
+                '_', '')
+            Humei_cleaned.loc['Night'] = int(match["Night"].iloc[0])
+            Humei_cleaned_list.append(Humei_cleaned)
 
-    # print('done with data')
+
+Humei_finaldata = Humei_cleaned_list
+df_humei = pd.DataFrame(Humei_cleaned_list)
+# df_humei['index'] = np.arange(start=0, stop=len(Humei_cleaned_list), step=1)
+
+df_humei = df_humei.reset_index(drop=True)
+print('done with data')

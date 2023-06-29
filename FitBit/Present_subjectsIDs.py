27a733baad6c6fee301dc920dba378e897bf1120
Shohreh_Wizard <30856578+shgh24@users.noreply.github.com>
2023-06-29 21:48:17 +0800

    FB data Dezambotti_FB_data_preparation_Step2_FB_SOL

--- FitBit/Present_subjectsIDs.py
+++ FitBit/Present_subjectsIDs.py
@@ -5,6 +5,11 @@ import os as os
 
 # Get the Oura subjects in each night and Hand
 Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/New'
+Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/SOL/Data'
+Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/SOL/Common_Subj_N1_Nodreem'
+
+# Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/SOL_FB/Common_Subj_N1_Nodreem'
+# Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/SOL/Data'
 # Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/FB/Data/June_13_2023'
 # "/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/Oura3/Data/combined_data_N01_L_2023_05_10.csv"
 fi = glob.glob(Saving_file_path+'/combined*.csv')

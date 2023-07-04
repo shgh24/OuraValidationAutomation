ef9a2d16a81a860d30005b1ba677abe662eec81f
Shohreh_Wizard <30856578+shgh24@users.noreply.github.com>
2023-07-04 15:15:03 +0800

    HPB watch scripts

--- HPB_watch/HPB_watch_step1.py
+++ HPB_watch/HPB_watch_step1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import glob as glob
 
@@ -6,6 +7,35 @@ import glob as glob
 HPB_raw_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/data_raw/HPB/CSC_sleep_intervals.xlsx'
 
 # reading the HPB file
-data = pd.read_excel(HPB_raw_path)
+data = pd.read_excel(HPB_raw_path, sheet_name="N1")
+stages = pd.unique(data["sleepstagecategory"])
+data["stage"] = data["sleepstagecategory"].map({
+    'LIGHT':
+    1,
+    'DEEP':
+    3,
+    'SAWAKE':
+    0,
+    'REM':
+    5,
+    'RESTLESS':
+    7,
+    'ASLEEP':
+    1,
+    'CAWAKE': 0})
 
-print('done')
+
+df_clean = pd.DataFrame(columns=["name",
+                                 "timestamp",
+                                 "predicted"])
+
+IDS = pd.unique(data["NUSID"])
+
+for id in IDS:
+
+    df_temp = data[(data["NUSID"] == id)]
+    result = df_temp.loc[df_temp.index.repeat(df_temp.intervaldurationmins*2)]
+
+    # df_clean["name"] =
+
+    print('done')

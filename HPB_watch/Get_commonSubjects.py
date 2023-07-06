d719fd5c578fa2b92fed5117863687c85f9888f0
Shohreh_Wizard <30856578+shgh24@users.noreply.github.com>
2023-07-06 09:29:00 +0800

    Hwawei watch added

---
+++ HPB_watch/Get_commonSubjects.py
@@ -0,0 +1,39 @@
+
+import os as os
+import glob
+import numpy as np
+import pandas as pd
+import os
+
+file = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/Subjects_For_FinalAnalysis.xlsx'
+
+
+subj_HPB_Nodreem = pd.read_excel(file, sheet_name='HPB_Nodreem')
+print('done')
+
+# devices = ['FB', 'Dreem', 'Oura3', 'Acti']
+devices = ['Oura3', 'FB', 'Acti']
+
+for devi in devices:
+    # Get the Oura subjects in each night and Hand
+
+    # data_dir = f'/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/{devi}/Data'
+    data_dir = f'/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/{devi}/Data/withHPB'
+    # /Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/HPB/Data
+    fi = glob.glob(data_dir + '/combined*N1*.csv')
+    # Subjects = pd.DataFrame()
+    for files in fi:
+        Subjects = pd.DataFrame()
+
+        data = pd.read_csv(files)
+        df1 = data
+        # df2 = subj_N1_All
+        df2 = subj_HPB_Nodreem
+
+        new_df = df1[df1['subject'].isin(df2['ID'])]
+
+        new_df.to_csv(files.replace('/withHPB',
+                                    '/withHPB/Common_Subj_N1_Nodreem'), index=False)
+
+
+print('done')
---
+++ HPB_watch/Present_subjectsIDs.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import glob
+import os as os
+
+# Get the Oura subjects in each night and Hand
+Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/HPB/Data/'
+Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/HPB/Data/Common_Subj_N1_Nodreem/'
+
+# Saving_file_path = '/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/Oura3/results/SOL'
+# "/Volumes/CSC5/SleepCognitionLab/Tera2b/Experiments/OuraValidation/Oura3/analysis/DeZambotti/Oura3/Data/combined_data_N01_L_2023_05_10.csv"
+fi = glob.glob(Saving_file_path+'/combined**.csv')
+# Subjects = pd.DataFrame()
+for files in fi:
+    Subjects = pd.DataFrame()
+
+    data = pd.read_csv(files)
+
+    Subjects['subj'] = data["subject"].unique()
+    Subjects.to_csv(files.replace('combined_data',
+                    'SubjectID'), index=False)
